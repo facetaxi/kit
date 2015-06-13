2e07679c1cb14bc7d0787e9e51037860db352c8d
gitkpi.ru <gitkpi@gmail.com>
2015-06-14 01:10:59 +0300

    >

---
+++ i/v_zserver_start_clear.php
@@ -0,0 +1,17 @@
+<?
+//VODITEL_ZOKNO = храним только те заказы которые удалены у водителя
+//стираем из инфоблока ID=15 записи водителя = это старт окна водителя
+if ( CSite::InGroup(array(7)) || CSite::InGroup(array(8)) || CSite::InGroup(array(9)) || CSite::InGroup(array(10)) ) {
+
+
+
+
+
+
+
+
+
+
+
+}
+?>
---
+++ i/yandexmap.php
@@ -0,0 +1,28 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");//клиенту
+?>
+
+ 
+<script type="text/javascript" charset="utf-8" src="prom.js"></script>
+
+<script>
+yandexinit1();
+</script>  
+ 
+ <table>
+ 
+ 
+ <tr>
+    <td align="center">
+    <div id="map" style="width:300px; height:300px" ></div>
+    </td>
+  </tr>
+ 
+</table> 
+
+ 
+
+ 
+
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
