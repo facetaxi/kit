933b20c42869e86a08721508cb021133a694fadb
Капитан Немо <gitkpi@gmail.com>
2015-06-17 17:49:24 +0300

    Create zakaz_ss.php

---
+++ i/zakaz_ss.php
@@ -0,0 +1,11 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+ 
+//$user = new CUser;
+//$fields = Array(
+  //"UF_SCREEN"          => $s
+  //);
+//$user->Update($USER->GetID(), $fields);
+
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
+?>
