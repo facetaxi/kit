e85d4d3dbc3b829eeab89888d5a1384dc9a0a659
Капитан Немо <gitkpi@gmail.com>
2015-06-18 21:01:41 +0300

    Update zakazi.php

--- i/zakazi.php
+++ i/zakazi.php
@@ -2,5 +2,26 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 
 
+//проверка активного заказа для тек-ого водителя
+							
+if ( CSite::InGroup(array(7)) || CSite::InGroup(array(8)) || CSite::InGroup(array(9)) || CSite::InGroup(array(10)) ) //для водителей
+	{
+		
+					global $USER; 
+					
+		   //откуда фильтр    
+					
+if(CModule::IncludeModule("iblock")) // вывод заказов для водителей = каждом/подъезду своё
+					{
+					
+					
+					
+
+					
+}
+ 			
+
+					
+
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");
 ?>
